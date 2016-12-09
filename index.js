9b58bcfb48d308c1b0bea52322accb4d6eb108bc
Menci <HuangHaoRui301@Gmail.com>
2016-12-09 16:40:55 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,219 @@
+const SANDBOX_DOCKER_IMAGE = 'menci/docker-sandbox';
+const SANDBOX_UID = 1111;
+const SANDBOX_GID = 1111;
+const SANDBOX_PATH = '/sandbox';
+const SANDBOX_EXEC_PATH = '/usr/sbin/sandbox';
+const SANDBOX_RESULT_PATH = '/root/result.txt';
+
+let Promise = require('bluebird');
+let Docker = require('dockerode');
+let TarStream = require('tar-stream');
+let path = require('path');
+let fs = Promise.promisifyAll(require('fs'));
+let streamToArray = require('stream-to-array');
+let docker = Promise.promisifyAll(new Docker());
+
+async function streamToBuffer(stream) {
+  return await new Promise((resolve, reject) => {
+    let buffers = [];
+    stream.on('data', buffer => {
+      buffers.push(buffer);
+    });
+
+    stream.on('end', () => {
+      let buffer = Buffer.concat(buffers);
+      resolve(buffer);
+    });
+
+    stream.on('error', reject);
+  });
+}
+
+async function tar(files) {
+  let pack = TarStream.pack();
+  for (let file of files) {
+    pack.entry(file, file.data);
+  }
+  pack.finalize();
+  return await streamToBuffer(pack);
+}
+
+async function untar(data) {
+  return await new Promise((resolve, reject) => {
+    let extract = TarStream.extract(), res = [];
+    extract.on('entry', async (header, stream, callback) => {
+      header.data = await streamToBuffer(stream);
+      res.push(header);
+      callback();
+    });
+
+    extract.write(data);
+    extract.end();
+
+    extract.on('finish', () => {
+      resolve(res);
+    });
+    extract.on('error', reject);
+  });
+}
+
+module.exports = async options => {
+  options = Object.assign({
+    program: '',
+    file_stdin: '',
+    file_stdout: '',
+    file_stderr: '',
+    time_limit: 0,
+    time_limit_reserve: 1,
+    memory_limit: 0,
+    memory_limit_reserve: 32 * 1024,
+    output_limit: 0,
+    process_limit: 0,
+    input_files: [],
+    output_files: []
+  }, options);
+
+  let container;
+  try {
+    // Check if the docker image exists
+    let image = Promise.promisifyAll(docker.getImage(SANDBOX_DOCKER_IMAGE));
+    try {
+      await image.inspectAsync();
+    } catch (e) {
+      // Image not exists
+      await new Promise((resolve, reject) => {
+        // Pull the image
+        docker.pull(SANDBOX_DOCKER_IMAGE, async (err, res) => {
+          if (err) reject(err);
+
+          // Check if the image is pulled
+          while (1) {
+            try {
+              await image.inspectAsync();
+              break;
+            } catch (e) {
+              // Delay 50ms
+              await Promise.delay(50);
+            }
+          }
+
+          resolve();
+        });
+      });
+    }
+
+    // Create the container
+    let container = await docker.createContainerAsync({
+      Image: SANDBOX_DOCKER_IMAGE,
+      HostConfig: {
+        NetworkMode: 'none'
+      }
+    });
+    Promise.promisifyAll(container);
+
+    // Start the container
+    let dataStart = await container.startAsync();
+
+    // Put the files via tar
+    options.input_files.push({
+      name: path.basename(options.program),
+      mode: parseInt('755', 8),
+      data: await fs.readFileAsync(options.program)
+    });
+    for (let file of options.input_files) {
+      file.uid = SANDBOX_UID;
+      file.gid = SANDBOX_GID;
+    }
+
+    await container.putArchiveAsync(await tar(options.input_files), {
+      path: SANDBOX_PATH
+    });
+
+    function getSandboxedPath(file) {
+      return path.join(SANDBOX_PATH, path.basename(file));
+    }
+
+    if (options.file_stdin.length) options.file_stdin = getSandboxedPath(options.file_stdin);
+    if (options.file_stdout.length) options.file_stdout = getSandboxedPath(options.file_stdout);
+    if (options.file_stderr.length) options.file_stderr = getSandboxedPath(options.file_stderr);
+
+    // Exec the program with sandbox
+    let exec = await container.execAsync({
+      Cmd: [
+        SANDBOX_EXEC_PATH,
+        getSandboxedPath(options.program),
+        options.file_stdin,
+        options.file_stdout,
+        options.file_stderr,
+        options.time_limit.toString(),
+        options.time_limit_reserve.toString(),
+        options.memory_limit.toString(),
+        options.memory_limit_reserve.toString(),
+        options.output_limit.toString(),
+        options.process_limit.toString(),
+        SANDBOX_RESULT_PATH
+      ],
+      AttachStdout: true,
+      AttachStderr: true
+    });
+    Promise.promisifyAll(exec);
+
+    let stream = await exec.startAsync();
+
+    // Wait for the exec
+    let dataExec;
+    do {
+      dataExec = await exec.inspectAsync();
+      await Promise.delay(50);
+    } while (dataExec.Running);
+
+    async function getFile(path) {
+      try {
+        let stream = await container.getArchiveAsync({
+          path: path
+        });
+
+        // Convert stream to buffer
+        let buffer = await streamToBuffer(stream);
+
+        let tar = await untar(buffer);
+
+        return tar[0];
+      } catch (e) {
+        return null;
+      }
+    }
+
+    let result = (await getFile('/root/result.txt')).data.toString();
+
+    let output_files = [];
+    for (let filename of options.output_files) {
+      output_files.push(await getFile(path.join(SANDBOX_PATH, filename)));
+    }
+
+    container.removeAsync({
+      force: true
+    }).then(() => {}).catch(() => {});
+
+    function parseResult(result) {
+      let a = result.split('\n');
+      return {
+        status: a[0],
+        debug_info: a[1],
+        time_usage: parseInt(a[2]),
+        memory_usage: parseInt(a[3])
+      };
+    }
+
+    return {
+      result: parseResult(result.toString()),
+      output_files: output_files
+    }
+  } catch (e) {
+    console.log(e);
+    container.removeAsync({
+      force: true
+    }).then(() => {}).catch(() => {});
+    throw e;
+  }
+};
---
+++ sandbox.cpp
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+#include <pwd.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+
+const pid_t SANDBOX_UID = 1111;
+const pid_t SANDBOX_GID = 1111;
+
+unsigned long parse_long(char *str) {
+    unsigned long x = 0;
+    for (char *p = str; *p; p++) x = x * 10 + *p - '0';
+    return x;
+}
+
+pid_t pid;
+long time_limit_to_watch;
+bool time_limit_exceeded_killed;
+
+void *watcher_thread(void *arg) {
+    sleep(time_limit_to_watch);
+    kill(pid, SIGXCPU);
+    time_limit_exceeded_killed = true;
+    return arg; // Avoid 'parameter set but not used' warning
+}
+
+int main(int argc, char **argv) {
+    if (argc != 11 + 1) {
+        fprintf(stderr, "Error: need 11 arguments\n");
+        fprintf(stderr, "Usage: %s program file_stdin file_stdout file_stderr time_limit time_limit_reserve memory_limit memory_limit_reserve output_limit process_limit file_result\n", argv[0]);
+        return 1;
+    }
+
+    if (getuid() != 0) {
+        fprintf(stderr, "Error: need root privileges\n");
+        return 1;
+    }
+
+    char *program = argv[1],
+         *file_stdin = argv[2],
+         *file_stdout = argv[3],
+         *file_stderr = argv[4],
+         *file_result = argv[11];
+    long time_limit = parse_long(argv[5]),
+         time_limit_reserve = parse_long(argv[6]),
+         memory_limit = parse_long(argv[7]),
+         memory_limit_reserve = parse_long(argv[8]),
+         output_limit = parse_long(argv[9]),
+         process_limit = parse_long(argv[10]);
+
+    time_limit_to_watch = time_limit + time_limit_reserve;
+
+#ifdef LOG
+    printf("Program: %s\n", program);
+    printf("Standard input file: %s\n", file_stdin);
+    printf("Standard output file: %s\n", file_stdout);
+    printf("Standard error file: %s\n", file_stderr);
+    printf("Time limit (seconds): %lu + %lu\n", time_limit, time_limit_reserve);
+    printf("Memory limit (kilobytes): %lu + %lu\n", memory_limit, memory_limit_reserve);
+    printf("Output limit (bytes): %lu\n", output_limit);
+    printf("Process limit: %lu\n", process_limit);
+    printf("Result file: %s\n", file_result);
+#endif
+
+    pid = fork();
+    if (pid > 0) {
+        // Parent process
+
+        FILE *fresult = fopen(file_result, "w");
+        if (!fresult) {
+            printf("Failed to open result file '%s'.", file_result);
+            return -1;
+        }
+
+        if (time_limit) {
+          pthread_t thread_id;
+          pthread_create(&thread_id, NULL, &watcher_thread, NULL);
+        }
+
+        struct rusage usage;
+        int status;
+        if (wait4(pid, &status, 0, &usage) == -1) {
+            fprintf(fresult, "Runtime Error\nwait4() = -1\n0\n0\n");
+            return 0;
+        }
+
+        if (WIFEXITED(status)) {
+            // Not signaled - exited normally
+            if (WEXITSTATUS(status) != 0) {
+                fprintf(fresult, "Runtime Error\nWIFEXITED - WEXITSTATUS() = %d\n", WEXITSTATUS(status));
+            } else {
+                fprintf(fresult, "Exited Normally\nWIFEXITED - WEXITSTATUS() = %d\n", WEXITSTATUS(status));
+            }
+        } else {
+            // Signaled
+            int sig = WTERMSIG(status);
+            if (sig == SIGXCPU || usage.ru_utime.tv_sec > time_limit) {
+                fprintf(fresult, "Time Limit Exceeded\nWEXITSTATUS() = %d, WTERMSIG() = %d (%s)\n", WEXITSTATUS(status), sig, strsignal(sig));
+            } else if (sig == SIGXFSZ) {
+                fprintf(fresult, "Output Limit Exceeded\nWEXITSTATUS() = %d, WTERMSIG() = %d (%s)\n", WEXITSTATUS(status), sig, strsignal(sig));
+            } else if (usage.ru_maxrss > memory_limit) {
+                fprintf(fresult, "Memory Limit Exceeded\nWEXITSTATUS() = %d, WTERMSIG() = %d (%s)\n", WEXITSTATUS(status), sig, strsignal(sig));
+            } else {
+                fprintf(fresult, "Runtime Error\nWEXITSTATUS() = %d, WTERMSIG() = %d (%s)\n", WEXITSTATUS(status), sig, strsignal(sig));
+            }
+        }
+
+#ifdef LOG
+        printf("memory_usage = %ld\n", usage.ru_maxrss);
+#endif
+        if (time_limit_exceeded_killed) fprintf(fresult, "%ld\n", time_limit_to_watch * 1000000);
+        else fprintf(fresult, "%ld\n", usage.ru_utime.tv_sec * 1000000 + usage.ru_utime.tv_usec);
+        fprintf(fresult, "%ld\n", usage.ru_maxrss);
+
+        fclose(fresult);
+    } else {
+#ifdef LOG
+        puts("Entered child process.");
+#endif
+
+        // Child process
+
+        if (time_limit) {
+            struct rlimit lim;
+            lim.rlim_cur = time_limit + time_limit_reserve;
+            lim.rlim_max = time_limit + time_limit_reserve;
+            setrlimit(RLIMIT_CPU, &lim);
+        }
+
+        if (memory_limit) {
+            struct rlimit lim;
+            lim.rlim_cur = (memory_limit + memory_limit_reserve) * 1024;
+            lim.rlim_max = (memory_limit + memory_limit_reserve) * 1024;
+            setrlimit(RLIMIT_AS, &lim);
+
+            lim.rlim_cur = memory_limit;
+            lim.rlim_max = memory_limit;
+            setrlimit(RLIMIT_STACK, &lim);
+        }
+
+        if (output_limit) {
+            struct rlimit lim;
+            lim.rlim_cur = output_limit;
+            lim.rlim_max = output_limit;
+            setrlimit(RLIMIT_FSIZE, &lim);
+        }
+
+        if (process_limit) {
+            struct rlimit lim;
+            lim.rlim_cur = process_limit + 1;
+            lim.rlim_max = process_limit + 1;
+            setrlimit(RLIMIT_NPROC, &lim);
+        }
+
+#ifdef LOG
+        puts("Entering target program...");
+#endif
+
+        chdir("/sandbox");
+
+        setuid(SANDBOX_UID);
+        setgid(SANDBOX_GID);
+
+        if (strlen(file_stdin)) freopen(file_stdin, "r", stdin);
+        else freopen("/dev/null", "r", stdin);
+
+        if (strlen(file_stdout)) freopen(file_stdout, "w", stdout);
+        else freopen("/dev/null", "w", stdout);
+
+        if (strlen(file_stderr)) freopen(file_stderr, "w", stderr);
+        else freopen("/dev/null", "w", stderr);
+
+        execlp(program, program, NULL);
+    }
+
+    return 0;
+}
